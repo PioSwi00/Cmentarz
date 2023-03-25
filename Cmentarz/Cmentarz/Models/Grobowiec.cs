e262f24fae005edd5fbe9fafb7b8899c0c92cece
Michal Motyka <michmot546@student.polsl.pl>
2023-03-25 13:05:53 +0100

    Grobowiec + Zmarły

---
+++ Cmentarz/Cmentarz/Models/Grobowiec.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cmentarz.Models
+{
+    [Table("Grobowce")]
+    public class Grobowiec
+    {
+        [Key]
+        public int IdGrobowiec { get; set; }
+        [Required]
+        public string IdWlasciciel { get; set; }
+        [Required]
+        public string Lokalizacja { get; set; }
+        public decimal Cena { get; set; }
+        public List<string>ListaOdwiedzajacy { get; set; }
+        [Required]
+        bool CzyZajety { get; set; }
+    }
+}
---
+++ Cmentarz/Cmentarz/Models/Zmarly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cmentarz.Models
+{
+    [Table("Zmarli")]
+    public class Zmarly
+    {
+        [Key]
+        public int IdZmarly { get; set; }
+        [Required]
+        public string Imie { get; set;}
+        [Required]
+        public string Nazwisko { get; set;}
+        [Required]
+        public DateTime DataUrodzenia { get; set; }
+        [Required]
+        public DateTime DataSmierci { get; set; }
+    }
+}
