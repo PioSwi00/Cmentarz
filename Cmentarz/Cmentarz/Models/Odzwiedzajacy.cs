fc979bb8b0f0974b8e9bdf635f5a507bf37edc78
Michal Motyka <michmot546@student.polsl.pl>
2023-03-25 12:30:34 +0100

    k

---
+++ Cmentarz/Cmentarz/Models/Odzwiedzajacy.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cmentarz.Models
+{
+    [Table("Cmentarz")]
+    public class Odzwiedzajacy
+    {
+        [Key]
+        public int IdOdzwiedzajacy { get; set; }
+        [Required]
+        public string Imie { get; set; }
+        [Required]
+        public string Nazwisko { get; set; }
+    }
+}
