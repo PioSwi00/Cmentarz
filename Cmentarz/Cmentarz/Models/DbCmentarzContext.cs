40a2c093dc15b92cbffea4600a163847d8d502fa
PioSwi00 <piotswi950@student.polsl.pl>
2023-03-25 16:14:44 +0100

    Przygotowane utworzenie bazy na podstawie kodu

--- Cmentarz/Cmentarz/Models/DbCmentarzContext.cs
+++ Cmentarz/Cmentarz/Models/DbCmentarzContext.cs
@@ -1,5 +1,6 @@
 ﻿using Cmentarz.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 
@@ -8,39 +9,50 @@ using System.Text;
 
 namespace Cmentarz.Models
 {
+    
+
     public class DbCmentarzContext :DbContext
     {
 
-        public DbCmentarzContext(DbContextOptions<DbCmentarzContext> options) : base(options)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Cmentarz;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        public DbCmentarzContext() 
+        {
+          
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<OdwiedzajacyGrobowce>()
                    .HasOne(o => o.Grobowiec)
                    .WithMany(op => op.Odwiedzajacy_Grobowce)
-                   .HasForeignKey(odi => odi.IdGrobowiec);
+                   .HasForeignKey(odi => odi.IdGrobowiec);          // n:m
 
             modelBuilder.Entity<OdwiedzajacyGrobowce>()
                    .HasOne(o => o.Odwiedzajacy)
                    .WithMany(op => op.Odwiedzajacy_Grobowce)
-                   .HasForeignKey(odi => odi.IdOdwiedzajacy);
+                   .HasForeignKey(odi => odi.IdOdwiedzajacy);       // odwidzajacy grobowiec
 
             modelBuilder.Entity<Wlasciciel>()
-                .HasOne(w => w.Uzytkownik)
+                .HasOne(w => w.Uzytkownik)                          //1:1 wlasciciel uzytkownik
                 .WithOne()
                 .HasForeignKey<Wlasciciel>(w => w.IdUzytkownik);
 
             modelBuilder.Entity<Wlasciciel>()
                 .HasMany(w => w.Lista_Grobowcow)
-                .WithOne(g => g.Wlasciciel)
+                .WithOne(g => g.Wlasciciel)                     //1:n Wlasciciel Grobowiec
                 .HasForeignKey(g => g.IdWlasciciel);
 
             modelBuilder.Entity<Grobowiec>()
-                .HasMany(z => z.ListaZmarlych)
+                .HasMany(z => z.ListaZmarlych)                  //1:n Grobowiec  Zmarly
                 .WithOne(g => g.Grobowiec)
                 .HasForeignKey(d => d.IdZmarly);
+          
+            modelBuilder.Entity<Odzwiedzajacy>()
+                .HasOne(o => o.Uzytkownik)
+                .WithOne(u => u.Odwiedzajacy)                       //1:1 odwiedzajac uzytkwonik
+                .HasForeignKey<Uzytkownik>(u => u.IdUzytkownik);
         }
 
         public DbSet<Uzytkownik> Uzytkownicy { get; set; }
@@ -50,8 +62,10 @@ namespace Cmentarz.Models
         public DbSet<Grobowiec> Grobowce { get; set; }
         public DbSet<OdwiedzajacyGrobowce> Odwiedzajacy_Grobowce{get;set;}
 
+       
+
     }
- 
+
 }
 
 
