af4aac54846517d4846eefe4a3f5eeaa68040116
michmot546 <72617993+michmot546@users.noreply.github.com>
2023-03-26 14:23:50 +0200

    Revert "test"

--- Cmentarz/Cmentarz/Models/DbCmentarzContext.cs
+++ Cmentarz/Cmentarz/Models/DbCmentarzContext.cs
@@ -10,11 +10,7 @@ namespace Cmentarz.Models
 {
     public class DbCmentarzContext :DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
 
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-        }
         public DbCmentarzContext(DbContextOptions<DbCmentarzContext> options) : base(options)
         {
 
@@ -45,14 +41,14 @@ namespace Cmentarz.Models
                 .WithOne(g => g.Grobowiec)
                 .HasForeignKey(d => d.IdZmarly);
             
-            modelBuilder.Entity<Odwiedzajacy>()
+            modelBuilder.Entity<Odzwiedzajacy>()
                 .HasOne(o => o.Uzytkownik)
                 .WithOne(u => u.Odwiedzajacy)
-                .HasForeignKey<Odwiedzajacy>(o => o.IdOdzwiedzajacy);
+                .HasForeignKey<Odzwiedzajacy>(o => o.IdOdzwiedzajacy);
         }
 
         public DbSet<Uzytkownik> Uzytkownicy { get; set; }
-        public DbSet<Odwiedzajacy> Odwiedzajacy { get; set; }
+        public DbSet<Odzwiedzajacy> Odwiedzajacy { get; set; }
         public DbSet<Wlasciciel> Wlasciciele { get; set; }
         public DbSet<Zmarly> Zmarli { get; set; }
         public DbSet<Grobowiec> Grobowce { get; set; }
