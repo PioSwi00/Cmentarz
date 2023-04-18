620c8c989e379549e482c2a3e190dd143045f2e6
PioSwi00 <piotswi950@student.polsl.pl>
2023-04-18 13:52:50 +0200

    IoW

--- Cmentarz/Cmentarz.DAL/Models/DbCmentarzContext.cs
+++ Cmentarz/Cmentarz.DAL/Models/DbCmentarzContext.cs
@@ -8,11 +8,19 @@ using System.Text;
 
 namespace Cmentarz.DAL.Models
 {
-    public class DbCmentarzContext : DbContext
+   public class DbCmentarzContext : DbContext
     {
-        public DbCmentarzContext(DbContextOptions<DbCmentarzContext> options) : base(options)
+        public DbSet<Uzytkownik> Uzytkownicy { get; set; }
+        public DbSet<Odwiedzajacy> Odwiedzajacy { get; set; }
+        public DbSet<Wlasciciel> Wlasciciele { get; set; }
+        public DbSet<Zmarly> Zmarli { get; set; }
+        public DbSet<Grobowiec> Grobowce { get; set; }
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
+          
+         optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Cmentarzysko;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); 
+            
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -22,29 +30,20 @@ namespace Cmentarz.DAL.Models
                 new Zmarly { IdZmarly = 2, Imie = "Anna", Nazwisko = "Nowak", DataUrodzenia = new DateTime(1980, 2, 2), DataSmierci = new DateTime(2022, 3, 22),GrobowiecID = 2 },
                 new Zmarly { IdZmarly = 3, Imie = "Piotr", Nazwisko = "Kaminski", DataUrodzenia = new DateTime(1970, 3, 3), DataSmierci = new DateTime(2022, 3, 24), GrobowiecID = 3}
             };
-
             modelBuilder.Entity<Wlasciciel>()
                         .HasData(new Wlasciciel { Imie = "Adam", Nazwisko = "Nowacki", IdWlasciciel = 1, Adres = "Jakis", IlGrobowcow = 2, Uzytkownik = new Uzytkownik { IdUzytkownik = 1, Login = "ss", Haslo = "ss", Grobowce = null, Odwiedzajacy = null, Wlasciciel = null }, Grobowce = new List<Grobowiec> { new Grobowiec { Cena = 2, IdGrobowiec = 1, IdWlasciciel = null, ListaOdwiedzajacy = null, Lokalizacja = "Test", Zmarli = zmarliList } } });
             modelBuilder.Entity<Zmarly>()
                          .HasData(zmarliList);
-
-
             modelBuilder.Entity<Grobowiec>()
                 .HasData(
                     new Grobowiec { Cena = 2, IdGrobowiec = 1, IdWlasciciel = 1, ListaOdwiedzajacy = null, Lokalizacja = "Test", Zmarli = zmarliList }
-
                 );*/
         }
 
-        public DbSet<Uzytkownik> Uzytkownicy { get; set; }
-        public DbSet<Odwiedzajacy> Odwiedzajacy { get; set; }
-        public DbSet<Wlasciciel> Wlasciciele { get; set; }
-        public DbSet<Zmarly> Zmarli { get; set; }
-        public DbSet<Grobowiec> Grobowce { get; set; }
-       
+
+
 
     }
- 
 }
 
 
--- Cmentarz/CmentarzWEB/Program.cs
+++ Cmentarz/CmentarzWEB/Program.cs
@@ -1,8 +1,10 @@
+using Cmentarz.DAL.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<DbCmentarzContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +24,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Zmarly}/{action=Index}/{id?}");
 
 app.Run();
